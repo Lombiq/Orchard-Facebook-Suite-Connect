d8da3fe1527808dbee453f01178b5437180cf230
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-11-27 17:10:07 +0100

    Code styling

--- FacebookConnectMigrations.cs
+++ FacebookConnectMigrations.cs
@@ -10,32 +10,35 @@ namespace Piedone.Facebook.Suite.Migrations
 
         public int Create() {
 			// Creating table FacebookConnectPartRecord
-			SchemaBuilder.CreateTable(typeof(FacebookConnectPartRecord).Name, table => table
-				.ContentPartRecord()
-				.Column<string>("Permissions")
-                .Column<bool>("AutoLogin")
-                .Column<bool>("OnlyAllowVerified")
+			SchemaBuilder.CreateTable(typeof(FacebookConnectPartRecord).Name, 
+                table => table
+				    .ContentPartRecord()
+				    .Column<string>("Permissions")
+                    .Column<bool>("AutoLogin")
+                    .Column<bool>("OnlyAllowVerified")
 			);
 
-            ContentDefinitionManager.AlterTypeDefinition("FacebookConnectWidget", cfg => cfg
-                .WithPart(typeof(FacebookConnectPart).Name)
-                .WithPart("WidgetPart")
-                .WithPart("CommonPart")
-                .WithSetting("Stereotype", "Widget"));
+            ContentDefinitionManager.AlterTypeDefinition("FacebookConnectWidget", 
+                cfg => cfg
+                    .WithPart(typeof(FacebookConnectPart).Name)
+                    .WithPart("WidgetPart")
+                    .WithPart("CommonPart")
+                    .WithSetting("Stereotype", "Widget"));
 
             // Creating table FacebookUserPartRecord
-            SchemaBuilder.CreateTable(typeof(FacebookUserPartRecord).Name, table => table
-                .ContentPartRecord()
-                .Column<long>("FacebookUserId")
-                .Column<string>("Name")
-                .Column<string>("FirstName")
-                .Column<string>("LastName")
-                .Column<string>("Link")
-                .Column<string>("FacebookUserName")
-                .Column<string>("Gender")
-                .Column<int>("TimeZone")
-                .Column<string>("Locale")
-                .Column<bool>("IsVerified")
+            SchemaBuilder.CreateTable(typeof(FacebookUserPartRecord).Name, 
+                table => table
+                    .ContentPartRecord()
+                    .Column<long>("FacebookUserId")
+                    .Column<string>("Name")
+                    .Column<string>("FirstName")
+                    .Column<string>("LastName")
+                    .Column<string>("Link")
+                    .Column<string>("FacebookUserName")
+                    .Column<string>("Gender")
+                    .Column<int>("TimeZone")
+                    .Column<string>("Locale")
+                    .Column<bool>("IsVerified")
             ).AlterTable(typeof(FacebookUserPartRecord).Name,
                 table => table
                     .CreateIndex("FacebookUser", new string[] { "FacebookUserId" })
@@ -68,15 +71,8 @@ namespace Piedone.Facebook.Suite.Migrations
         public int UpdateFrom3()
         {
             // Because of the splitting of Connect and the rest of Facebook Suite, this rename is necessary
-            try
-            {
-                SchemaBuilder.ExecuteSql("EXEC sp_rename 'Piedone_Facebook_Suite_FacebookConnectPartRecord', 'Piedone_Facebook_Suite_Connect_FacebookConnectPartRecord'");
-                SchemaBuilder.ExecuteSql("EXEC sp_rename 'Piedone_Facebook_Suite_FacebookUserPartRecord', 'Piedone_Facebook_Suite_Connect_FacebookUserPartRecord'");
-            }
-            catch (System.Exception)
-            {
-                // The tables were already created with the new name (so this is a fresh installation)
-            }
+            SchemaBuilder.ExecuteSql("EXEC sp_rename 'Piedone_Facebook_Suite_FacebookConnectPartRecord', 'Piedone_Facebook_Suite_Connect_FacebookConnectPartRecord'");
+            SchemaBuilder.ExecuteSql("EXEC sp_rename 'Piedone_Facebook_Suite_FacebookUserPartRecord', 'Piedone_Facebook_Suite_Connect_FacebookUserPartRecord'");
 
             return 4;
         }
