ad21c0ef5d925a4dc6308b52d68fab635428f19f
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-02-24 19:08:16 +0100

    Display link to the FB profile on profile page

---
+++ Drivers/FacebookUserPartDriver.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+using Piedone.Facebook.Suite.Models;
+
+namespace Piedone.Facebook.Suite.Drivers
+{
+    [OrchardFeature("Piedone.Facebook.Suite.Connect")]
+    public class FacebookUserPartDriver : ContentPartDriver<FacebookUserPart>
+    {
+        protected override DriverResult Display(FacebookUserPart part, string displayType, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_FacebookUser",
+                () => shapeHelper.Parts_FacebookUser());
+        }
+    }
+}
